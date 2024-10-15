69dbbdda3d998a40003c70750c20c84cb9dfa357
tomo-0224 <mail@tomokinishimura.com>
2024-10-15 15:17:16 +0900

    ✨ メンバーページの更新途中

--- app/members/page.tsx
+++ app/members/page.tsx
@@ -1,3 +1,17 @@
+const data = {
+  contents: [
+    {
+      id: "1",
+      image: {
+        url: "/img-member1.jpg",
+        width: 240,
+        height: 240,
+      },
+      name:
+    },
+  ],
+};
+
 export default function Page() {
   return (
     <div style={{ margin: 20 }}>

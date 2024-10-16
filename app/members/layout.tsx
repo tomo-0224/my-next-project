e2bd757b77f60756db0bf638c862880a9f18c28c
tomo-0224 <mail@tomokinishimura.com>
2024-10-16 20:12:30 +0900

    ✨ メンバーページレイアウト調整

--- app/members/layout.tsx
+++ app/members/layout.tsx
@@ -1,13 +1,9 @@
-import styles from "./page.module.css";
+import Sheet from "@/app/_components/Sheet";
 
 type Props = {
   children: React.ReactNode;
 };
 
 export default function RootLayout({ children }: Props) {
-  return (
-    <>
-      <div className={styles.container}>{children}</div>
-    </>
-  );
+  return <Sheet>{children}</Sheet>;
 }

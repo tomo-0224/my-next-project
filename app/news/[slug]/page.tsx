01d687721df6a4c942a3731d53b589fc12c43c0f
tomo-0224 <mail@tomokinishimura.com>
2024-10-26 15:16:08 +0900

    🎨 ニュースにSSRを適用

--- app/news/[slug]/page.tsx
+++ app/news/[slug]/page.tsx
@@ -13,6 +13,7 @@ type Props = {
   };
 };
 
+export const revalidate = 0;
 export default async function Page({ params, searchParams }: Props) {
   const data = await getNewsDetail(params.slug, {
     draftKey: searchParams.dk,
